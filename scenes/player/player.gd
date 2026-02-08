0f6f9a0595b37938315bbd44f891777c2ea75bdd
WMinerva <nicolas.ochoa1697@gmail.com>
2026-02-07 20:15:11 -0400

    jump

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,26 @@
 extends CharacterBody2D
 
 @export var speed: int
+@export var jump_speed: int
+@export var gravity : int
+var is_facing_right = true
 
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_speed
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
 	move_x()
+	flip()
 	move_and_slide()
+
+
+func flip():
+	if (is_facing_right and velocity.x < 0) or (not is_facing_right and velocity.x > 0):
+		scale.x *= -1
+		is_facing_right = not is_facing_right
 
 
 func move_x():
