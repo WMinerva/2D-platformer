9932c8f51740c070e35dcc83734ad6d51e3e8963
WMinerva <nicolas.ochoa1697@gmail.com>
2026-02-26 00:41:09 -0400

    bat follow ai

--- scenes/enemies/bat.gd
+++ scenes/enemies/bat.gd
@@ -3,7 +3,16 @@ extends Enemy
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	if not player:
+		return
 	flip()
+	follow_player()
 	
 func flip():
 	animated_sprite_2d.flip_h = player.position.x > position.x
+
+func follow_player():
+	var direction = player.position - position
+	direction = direction.normalized()
+	velocity = direction * speed
+	move_and_slide()
