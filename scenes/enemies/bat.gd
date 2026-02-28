af9443a13adfc2007f5c54a23b9f1257cc297dd6
WMinerva <nicolas.ochoa1697@gmail.com>
2026-02-27 20:20:02 -0400

    animated enemies die

--- scenes/enemies/bat.gd
+++ scenes/enemies/bat.gd
@@ -1,6 +1,5 @@
 extends Enemy
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if not player:
--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var speed: int
 @export var gravity: int
 @onready var hurt_box: Area2D = $HurtBox
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 var player
 
@@ -14,4 +17,10 @@ func _ready() -> void:
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	
 	if player.position.y < hurt_box.global_position.y:
-		queue_free()
+		animation_player.play("dissapear")
+		set_physics_process(false)
+		animated_sprite_2d.pause()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	queue_free()
